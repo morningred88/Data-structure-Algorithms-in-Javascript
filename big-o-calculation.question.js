efec6ba8abf786deb70204c55874a35734665be7
Xiaoli Ma <77655405+morningred88@users.noreply.github.com>
2021-09-05 22:03:46 +0000

    Added big O calculation

---
+++ big-o-calculation.question.js
@@ -0,0 +1,12 @@
+// What is the Big O of the below function? (Hint, you may want to go line by line)
+function (input){
+  let a=10; 
+  a=50+3;
+  for (let i=0; i<input.length;i++){
+    aotherFuntion();
+    let stranger =true;
+    a++;
+  }
+  return a;
+}
+
