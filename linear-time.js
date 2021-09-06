89228d641f76df8f76606b12e57f4045437706eb
Lily Ma <77655405+morningred88@users.noreply.github.com>
2021-09-06 02:03:00 +0000

    Linear time added

---
+++ linear-time.js
@@ -0,0 +1,11 @@
+//O(n) - linear time 
+//Two arrays with the same indentation
+function compressBoxesTwice (boxes1, boxes2){
+  boxes1.forEach (function(boxes1){
+    CONSOLE.LOG(boxes1);
+  });
+  
+  boxes2.forEach (function(boxes2){
+    CONSOLE.LOG(boxes2);
+  });
+}
